d1b85d206730f647c21ae3aa1c544e3a7c7dcc4c
Jack Long <putheara.long@gmail.com>
2020-03-16 14:25:41 -0400

    got uv index to show on html page

--- myScript.js
+++ myScript.js
@@ -39,12 +39,34 @@ $(document).ready(function(){
        eventArray = []}
     else eventArray = JSON.parse(localStorage.getItem("occasion"));};
     // var storedArray = JSON.parse(localStorage.getItem("occasion"))*/
-    
+
+    function getAPIKey (APIKey) {
+      var defaultAPIKey = "0ef2ac94f5fde5899714a4ccfc6bf24c";
+      
+      $("#apiKeyBttn").click(function(event){
+        event.preventDefault();
+        var userAPIKey = $("#inputBox").val();
+        console.log(userAPIKey);
+        if (userAPIKey === "") {
+          APIKey = defaultAPIKey;
+        }
+        else 
+         APIKey = userAPIKey;
+
+        })
+ 
+        return(APIKey);
+      
+    };
+  //});
+    //console.log(userAPIKey);
     $("#searchBttn").click(function(event){
     event.preventDefault();
+    $(".infoDisplay").empty();
+    getAPIKey(APIKey);
     
     var searchCity = $("#searchBox").val();
-    var APIKey = "0ef2ac94f5fde5899714a4ccfc6bf24c";
+    
     
    
 
@@ -57,18 +79,34 @@ $(document).ready(function(){
       method: "GET"
     })
       // We store all of the retrieved data inside of an object called "response"
-      .then(function(response) {
-        var iconCode = response.weather[0].icon;
+      .then(function(response1) {
+        var iconCode = response1.weather[0].icon;
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         var iconImage = "<image src=" + iconUrl + ">";
-        
-        console.log (iconUrl);
-        
+        var p1 = $("<p>").html("<h3><b>" + response1.name +" ("+ todayDate +")" + iconImage + "</b></h3");
+        var p2 = $("<p>").html("<h5><b>Temperature: " + response1.main.temp + "&degF</b></h5>");
+        var p3 = $("<p>").html("<h5><b>Humidity: " + response1.main.humidity + "</b></h5>");
+        var cityLat = response1.coord.lat;
+        var cityLon = response1.coord.lon;
+        var uvIndexURL= "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + cityLat + "&lon=" + cityLon;
         // Transfer content to HTML
-        $("#city").html(response.name +"("+ todayDate +")" + iconImage);
-        $("#temp").html("Temperature: " + response.main.temp + "&degF");
-        $("#humidity").html("Humidity: " + response.main.humidity);
-        console.log(response);
+        $("#city").append(p1);
+        $("#temp").append(p2);
+        $("#humidity").append(p3);
+         
+    $.ajax({
+      url: uvIndexURL,
+     method: "GET"
+    })
+      // We store all of the retrieved data inside of an object called "response"
+     .then(function(response2) {
+      var p4 = $("<p>").html("<h5><b>UV Index: " + response2.value + "</b></h5>");
+     $("#uv-index").append(p4);
+    
+    });
+
+    //console.log(response2);
+
     })})});
     
     
